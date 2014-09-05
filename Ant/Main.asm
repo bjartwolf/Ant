718bed9a77e83f99e28e68c09c9299f5bee6ae30
Bjartwolf <bjartnes@gmail.com>
2014-09-05 11:48:31 +0200

    Iterator counting bug

--- Ant/Main.asm
+++ Ant/Main.asm
@@ -3,92 +3,92 @@ baseMSB = #$20
 baseLSB = #$00
 videoadrLSB = $fa
 videoadrMSB = $fb
-regbase = $d000                             ; base adress for registermainpulation 
-whiteblack = #$f0                           ; The colors the fields should have for on/off 
+regbase = $d000                             ; base adress for registermainpulation  
+whiteblack = #$F0                           ; The colors the fields should have for on/off  
 videocolorbase = $0400
-dir = $f3                                   ; ant dir         
-right = #0              ; using 0 for right and adding 64 when turning left 
-up = #64                ; This allows for wrapping around automatically 
+dir = $f3                                   ; ant dir          
+right = #0              ; using 0 for right and adding 64 when turning left  
+up = #64                ; This allows for wrapping around automatically  
 left = #128
 down = #192
-antPosByteLSB = $ea         ; which byte is ant in
+antPosByteLSB = $ea                         ; which byte is ant in 
 antPosByteMSB = $eb
-antPosInByte = $e8		; position of ant within byte
+antPosInByte = $e8                          ; position of ant within byte 
 
-start                   ; Configure HI RES display          
-                        lda #$3b            ; Bit 5 on                 
-                        sta regbase + 17    ; Reg 17 Bit 5 enable high res                   
-                        lda #$18            ; Point to high res memory map                  
-                        sta regbase + 24    ; Reg 24                    
+start                   ; Configure HI RES display           
+                        lda #$3b            ; Bit 5 on                  
+                        sta regbase + 17    ; Reg 17 Bit 5 enable high res                    
+                        lda #$18            ; Point to high res memory map                   
+                        sta regbase + 24    ; Reg 24                     
 
                         lda baseLSB
                         sta videoadrLSB
                         lda baseMSB
                         sta videoadrMSB
 
-                        ; Set color in 25*40 grid to black and white        
-                        ; For each character we can set the color we should have for on and off 
-                        lda whiteblack      ; msb nybble is on color, lsb is off, 4 bit colors           
-                        ; Easier and faster than using 16 bit adressing - split into four. 
-                        ldy #250            ; Count from 250                 
+                        ; Set color in 25*40 grid to black and white         
+                        ; For each character we can set the color we should have for on and off  
+                        lda whiteblack      ; msb nybble is on color, lsb is off, 4 bit colors            
+                        ; Easier and faster than using 16 bit adressing - split into four.  
+                        ldy #250            ; Count from 250                  
 resetcolorscheme        dey 
-                        sta videocolorbase,y  ; 0-249 
-                        sta videocolorbase + $00FA,y  ; 250-499  
-                        sta videocolorbase + $01F3,y  ; 499-749 
-                        sta videocolorbase + $02EE,y  ; 750-999 
+                        sta videocolorbase,y  ; 0-249  
+                        sta videocolorbase + $FA,y  ; 250-499   
+                        sta videocolorbase + $01F4,y  ; 500-749  
+                        sta videocolorbase + $02EE,y  ; 750-999  
                         bne resetcolorscheme
 
-                        ; must clear all bits from $20 to $40 
-                        ; We have 320*200/8 = 8000 bytes to clear 
-                        ; in loops of 256 bytes = 31 
-                        lda #0              ; turn all bits in bitmap off                 
-                        ldy #0              ; clear y (iterator)		                 
-resetscreenmem          sta (videoadrLSB),y  ; Store in fb,fa location+y                 
+                        ; must clear all bits from $20 to $40  
+                        ; We have 320*200/8 = 8000 bytes to clear  
+                        ; in loops of 256 bytes = 31  
+                        lda #0              ; turn all bits in bitmap off                  
+                        ldy #0              ; clear y (iterator)		                  
+resetscreenmem          sta (videoadrLSB),y  ; Store in fb,fa location+y                  
                         iny 
                         bne resetscreenmem
-                        ldx videoadrMSB     ; load msb of loop range                 
-                        inx                 ; inx                  
-                        stx videoadrMSB     ; save stored value back                 
-                        cpx #$40	        ; we count to 3fff (I think, if we count to far it is only sprite memory I think)                   
+                        ldx videoadrMSB     ; load msb of loop range                  
+                        inx                 ; inx                   
+                        stx videoadrMSB     ; save stored value back                  
+                        cpx #$40            ; we count to 3fff (I think, if we count to far it is only sprite memory I think)                    
                         bne resetscreenmem
 
-					    ; store dir in f3             
-                        lda #0              ; dir 0            
+                        ; store dir in f3              
+                        lda #0              ; dir 0             
                         sta dir
-                        ; Set bitflag and xy position   
+                        ; Set bitflag and xy position    
                         lda #%00010000
                         sta antPosInByte
-                        lda #$2f            ;position middle    
+                        lda #$2f            ;position middle     
                         sta antPosByteMSB
-                        lda #$a3            ;position middle    
+                        lda #$a3            ;position middle     
                         sta antPosByteLSB
-                        ; flip color by xor with bit               
-loop                    lda antPosInByte      ; load bit flag for which bit to turn on                
-                        ldy #0              ; not sure how to do without index               
-                        eor (antPosByteLSB),y   ; use eor to flip value of black and white            
-                        sta (antPosByteLSB),y   ; store new color       
-                        and antPosInByte      ; only check current bit              
-                        cmp #0              ; check if black now            
-                        bne white           ; go to white if not equal(double check logic after three beers)             
-                        ; did not branch on white, so we are on black   
-                        lda dir             ; load directions into accumulator       
+                        ; flip color by xor with bit                
+loop                    lda antPosInByte    ; load bit flag for which bit to turn on                 
+                        ldy #0              ; not sure how to do without index                
+                        eor (antPosByteLSB),y  ; use eor to flip value of black and white             
+                        sta (antPosByteLSB),y  ; store new color        
+                        and antPosInByte    ; only check current bit               
+                        cmp #0              ; check if black now             
+                        bne white           ; go to white if not equal(double check logic after three beers)              
+                        ; did not branch on white, so we are on black    
+                        lda dir             ; load directions into accumulator        
                         clc 
-                        adc #64             ; Turn right is adding 64   
+                        adc #64             ; Turn right is adding 64    
                         sta dir
                         jmp checkdir
-white                   lda dir             ; load directions into accumulator   
+white                   lda dir             ; load directions into accumulator    
                         sec 
-                        sbc #64             ; turn left is subtracing 64   
+                        sbc #64             ; turn left is subtracing 64    
                         sta dir
-checkdir                cmp #192            ; acc already holds direction, comparing <= 192  
-                        bcs godown          ;   
-                        cmp #128            ; <= 128  
+checkdir                cmp #192            ; acc already holds direction, comparing <= 192   
+                        bcs godown          ;    
+                        cmp #128            ; <= 128   
                         bcs goleft
                         cmp #64
-                        bcs goup            ; fall through to right    
+                        bcs goup            ; fall through to right     
 goright                 lda antPosInByte
                         cmp #%00000001
-                        bne rightloop       ; Change bitflag to other side and increase memlocation by 8    
+                        bne rightloop       ; Change bitflag to other side and increase memlocation by 8     
                         lda #%10000000
                         sta antPosInByte
                         clc 
@@ -102,23 +102,23 @@ goright                 lda antPosInByte
 rightloop               lsr antPosInByte
                         jmp loop
 godown                  lda antPosByteLSB
-                        and #$07            ; and sets flag if result is zero   
-                        bne decScrMem       ; branch if three last bits is not zero   
-                        sec                 ; result is zero, set carry to borrow     
-                        lda antPosByteLSB       ; must go down 320-7=313=0x139   
+                        and #$07            ; and sets flag if result is zero    
+                        bne decScrMem       ; branch if three last bits is not zero    
+                        sec                 ; result is zero, set carry to borrow      
+                        lda antPosByteLSB   ; must go down 320-7=313=0x139    
                         sbc #$39
                         sta antPosByteLSB
                         lda antPosByteMSB
                         sbc #1
                         sta antPosByteMSB
                         jmp loop
-decScrMem               dec antPosByteLSB       ; ca not overflow because it is nnot zer     
+decScrMem               dec antPosByteLSB   ; ca not overflow because it is nnot zer      
                         jmp loop
 goup                    lda antPosByteLSB
                         and #7
                         cmp #7
-                        bne incScrMem       ; if 7&y != 7 then take loop     
-                        ; 7 - should go 313 more, 139 in hex    
+                        bne incScrMem       ; if 7&y != 7 then take loop      
+                        ; 7 - should go 313 more, 139 in hex     
                         clc 
                         lda #$39
                         adc antPosByteLSB
@@ -127,12 +127,12 @@ goup                    lda antPosByteLSB
                         adc #1
                         sta antPosByteMSB
                         jmp loop
-incScrMem               inc antPosByteLSB       ; can not overflow as not 7    
+incScrMem               inc antPosByteLSB   ; can not overflow as not 7     
                         jmp loop
 goleft                  lda antPosInByte
                         cmp #%10000000
                         bne leftloop
-                        lda #%00000001      ; maybe do in place rotate of flag without carry?   
+                        lda #%00000001      ; maybe do in place rotate of flag without carry?    
                         sta antPosInByte
                         sec 
                         lda antPosByteLSB
