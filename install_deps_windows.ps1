6202238dfc68add5130e94dd6eccfefbbf2cd5ab
Bjorn Einar Bjartnes <bjartnes@gmail.com>
2022-07-17 20:11:23 +0200

    Download some deps..

---
+++ install_deps_windows.ps1
@@ -0,0 +1,11 @@
+wget https://www.badgerpunch.com/booster/kickass.zip -OutFile kickass.zip
+wget https://www.badgerpunch.com/booster/vice.zip -OutFile vice.zip
+
+# Not so sure this works...
+wget https://sourceforge.net/projects/c64-debugger/files/C64%2065XE%20NES%20Debugger%20v0.64.58.6.win32.zip -OutFile debugger.zip
+Expand-Archive -DestinationPath tools kickass.zip
+Expand-Archive -DestinationPath tools vice.zip
+Expand-Archive -DestinationPath tools/debugger debugger.zip
+del kickass.zip
+del vice.zip
+del debugger.zip
